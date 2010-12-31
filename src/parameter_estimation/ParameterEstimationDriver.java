dedc3c131b3516410d3f1eb784d30eac5486a265
unknown <nmvdewie@.UGent.be>
2010-12-31 13:41:03 +0100

    ParameterEstimationDriver refactored
    reader methods are extracted according to the
    part of the INPUT file they read

--- src/parameter_estimation/ParameterEstimationDriver.java
+++ src/parameter_estimation/ParameterEstimationDriver.java
@@ -1,7 +1,9 @@
 package parameter_estimation;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Map;
 
@@ -14,6 +16,13 @@ import org.apache.log4j.SimpleLayout;
 public class ParameterEstimationDriver {
 	public static Logger logger = Logger.getLogger(ParameterEstimationDriver.class);
 	static int mode;
+	static Chemistry chemistry;
+	static Experiments experiments;
+	static Paths paths;
+	static ReactorInputCollector reactorInputCollector;
+	static Fitting fitting;
+	static Licenses licenses;
+	static Parameters2D params;
 	//this comment is added to verify version control system
 	/**
 	 * @param args
@@ -23,13 +32,78 @@ public class ParameterEstimationDriver {
 	public static void main(String[] args) throws Exception {
 		long time = System.currentTimeMillis();
 		initializeLog();
-		Chemistry chemistry = new Chemistry();
-		ReactorInputCollector reactorInputCollector = new ReactorInputCollector();
-		Experiments experiments = new Experiments();
-		Paths paths = new Paths();
-		Fitting fitting = new Fitting();
+		chemistry = new Chemistry();
+		reactorInputCollector = new ReactorInputCollector();
+		experiments = new Experiments();
+		paths = new Paths();
+		fitting = new Fitting();
 
 		BufferedReader in  = null;
+		in = readINPUT(args);
+		
+		readPaths(in);
+
+		readLicenses(in);
+
+		readChemistryInput(in);
+
+		readExperimentalDBPaths(in);
+
+		readReactorDBInfo(in);
+
+		readNoExperiments(in);
+		
+		//set no. of regular experiments:
+		reactorInputCollector.setNoRegularExperiments(reactorInputCollector.getTotalNoExperiments()-reactorInputCollector.getNoIgnitionDelayExperiments() - reactorInputCollector.getNoFlameSpeedExperiments());
+
+		int noParams = readOptimizationFlags(in);
+
+		readMode(in);
+
+		readReactorInputNames(in);	
+		
+		//insert ReactorInputCollector in Experiments:
+		experiments.setReactorInputCollector(reactorInputCollector);
+
+		//OPTIMIZATION SECTION: modified Arrhenius parameters [A, n, Ea]: 1 = loose, parameter will be varied; 0 = fixed, parameter will be fixed
+		in.readLine();
+
+		int noFittedReactions = readOptimizedReactions(in, noParams);
+
+		in.close();
+
+
+		setParameterBoundaries(noFittedReactions);
+
+		switch(mode){
+		case 0:	logger.info("PARITY PLOT MODE");	
+		Param_Est p0 = new Param_Est(paths, chemistry, experiments, fitting, licenses);
+		p0.parity();
+		//Function f = new Function(p0.getModelValues(),p0.getExp());
+		//logger.info("SSQ is: "+f.getSRES());
+		break;
+		case 1:	logger.info("PARAMETER OPTIMIZATION MODE");		
+		Param_Est p1 = new Param_Est(paths, chemistry, experiments, fitting, licenses);
+		p1.optimizeParameters();
+		p1.statistics();
+		p1.parity();
+		break;
+		case 2: logger.info("EXCEL POSTPROCESSING MODE");
+		Param_Est p2 = new Param_Est(paths, chemistry, experiments, licenses);
+		p2.excelFiles();
+		break;
+		case 3: logger.info("STATISTICS MODE");
+		Param_Est p3 = new Param_Est(paths, chemistry, experiments, fitting, licenses);
+		p3.statistics();
+		p3.parity();
+		}
+		long timeTook = (System.currentTimeMillis() - time)/1000;
+		logger.info("Time needed for this program to finish: (sec) "+timeTook);
+	}
+
+	private static BufferedReader readINPUT(String[] args)
+			throws FileNotFoundException {
+		BufferedReader in;
 		if(args.length == 0){
 			//input file will be searched in working directory under the name INPUT.txt:
 			in = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/INPUT.txt"));	
@@ -38,42 +112,10 @@ public class ParameterEstimationDriver {
 			//specify user-defined input file:
 			in = new BufferedReader(new FileReader(args[0]));
 		}
-		
-		in.readLine();
-		String workingDir = in.readLine();
-		if (workingDir.charAt(workingDir.length()-1)!='/'){
-			logger.debug("Pathname to working directory needs to end with forward slash '/' !");
-			System.exit(-1);
-		}
-		else {
-			paths.setWorkingDir(workingDir);
-		}
+		return in;
+	}
 
-		//Chemkin Directory:
-		in.readLine();
-		paths.setChemkinDir(in.readLine());
-
-		//Number of chemkin licenses to be used:
-		in.readLine();
-		int noLicenses = Integer.parseInt(in.readLine());
-		Licenses licenses = new Licenses(new Integer(noLicenses));
-
-		//Chemistry input filename:
-		in.readLine();
-		chemistry.setChemistryInput(in.readLine());
-
-		//Experimental database:(leave blank line if file is not required for run mode)
-		in.readLine();		
-		experiments.setPathExperimentalDB(in.readLine());
-
-		//Ignition Delay database:(leave blank line if file is not required for run mode)
-		in.readLine();
-		experiments.setPathIgnitionDB(in.readLine());
-
-		//flame speed database:(leave blank line if file is not required for run mode)
-		in.readLine();
-		experiments.setPathFlameSpeedDB(in.readLine());
-
+	private static void readReactorDBInfo(BufferedReader in) throws IOException {
 		//user specified reactor setup data in form of separate files(false) or one single reactor setup database (true)?
 		in.readLine();
 		experiments.setFlagReactorDB(Boolean.parseBoolean(in.readLine()));
@@ -85,41 +127,63 @@ public class ParameterEstimationDriver {
 		//Reactor setup method: 1: standard, 2: new method with isothermal, isobaric reactor profiles, but variable reactor lengths (type 0 if not applicable)
 		in.readLine();
 		experiments.setFlagReactorSetupType(new Integer(in.readLine()));
+	}
 
-		//total number of experiments:
+	private static void readChemistryInput(BufferedReader in)
+			throws IOException {
+		//Chemistry input filename:
 		in.readLine();
-		reactorInputCollector.setTotalNoExperiments(new Integer(in.readLine()));
+		chemistry.setChemistryInput(in.readLine());
+	}
 
-		//number of experiments in which ignition delay is the response variable
+	private static void setParameterBoundaries(int noFittedReactions) {
+		double [][] betaMin = new double [noFittedReactions][Chemistry.getNoparametersperreaction()];
+		double [][] betaMax = new double [noFittedReactions][Chemistry.getNoparametersperreaction()];
+		for (int i = 0; i < noFittedReactions; i++) {
+			for (int j = 0; j < Chemistry.getNoparametersperreaction(); j++){
+				betaMin[i][j]=0;
+				betaMax[i][j]=1e20;
+			}
+		}
+
+		params.setBetaMin(betaMin);
+		params.setBetaMax(betaMax);
+
+
+		chemistry.setParams(params);
+	}
+
+	private static int readOptimizedReactions(BufferedReader in, int noParams)
+			throws IOException {
+		//number of reactions that will be optimized:
 		in.readLine();
-		reactorInputCollector.setNoIgnitionDelayExperiments(new Integer(in.readLine()));
-		
-		
-		//number of experiments in which flame speed is the response variable
-		in.readLine();
-		reactorInputCollector.setNoFlameSpeedExperiments(new Integer(in.readLine()));
-		
-		//set no. of regular experiments:
-		reactorInputCollector.setNoRegularExperiments(reactorInputCollector.getTotalNoExperiments()-reactorInputCollector.getNoIgnitionDelayExperiments() - reactorInputCollector.getNoFlameSpeedExperiments());
+		int noFittedReactions = Integer.parseInt(in.readLine());
 
-		//number of parameters to be fitted:
-		in.readLine();
-		int noParams = Integer.parseInt(in.readLine());
+		int [][] fixRxns = new int [noFittedReactions][chemistry.getNoparametersperreaction()];
+		for (int i = 0; i < noFittedReactions; i++){
+			//comment line with "reaction i: "
+			in.readLine();
+			// string of 1,0,1:
+			String [] s = in.readLine().split(",");
+			for (int j = 0; j < chemistry.getNoparametersperreaction(); j++){
+				fixRxns[i][j] = Integer.parseInt(s[j]);
+			}
+		}
+		params = new Parameters2D(fixRxns);
 
-		//optimization flags:
-		in.readLine();
-		fitting.setFlagRosenbrock(new Boolean(in.readLine()));
+		boolean flagNoParameters = checkNoParameters(fixRxns, noParams);
+		if (!flagNoParameters) {
+			logger.debug("Number of parameters to be fitted provided in INPUT.txt does not equal the number of ones you specified in the optimization section in INPUT.txt!");
+			System.exit(-1);
+		}
+		else {
+			//do nothing, continue
+		}
+		return noFittedReactions;
+	}
 
-
-		in.readLine();
-		fitting.setFlagLM(new Boolean(in.readLine()));
-
-		in.readLine();
-		fitting.setMaxNoEvaluations(new Integer(in.readLine()));
-
-		in.readLine();
-		mode = Integer.parseInt(in.readLine());
-
+	private static void readReactorInputNames(BufferedReader in)
+			throws IOException {
 		//REACTOR INPUT FILE NAMES:
 		in.readLine();
 
@@ -168,81 +232,84 @@ public class ParameterEstimationDriver {
 				reactorInputCollector.setFlameSpeedInputs(inputs);
 			}
 
-		}	
-		
-		//insert ReactorInputCollector in Experiments:
-		experiments.setReactorInputCollector(reactorInputCollector);
-
-		//OPTIMIZATION SECTION: modified Arrhenius parameters [A, n, Ea]: 1 = loose, parameter will be varied; 0 = fixed, parameter will be fixed
-		in.readLine();
-
-		//number of reactions that will be optimized:
-		in.readLine();
-		int noFittedReactions = Integer.parseInt(in.readLine());
-
-		int [][] fixRxns = new int [noFittedReactions][chemistry.getNoparametersperreaction()];
-		for (int i = 0; i < noFittedReactions; i++){
-			//comment line with "reaction i: "
-			in.readLine();
-			// string of 1,0,1:
-			String [] s = in.readLine().split(",");
-			for (int j = 0; j < chemistry.getNoparametersperreaction(); j++){
-				fixRxns[i][j] = Integer.parseInt(s[j]);
-			}
 		}
-		Parameters2D params = new Parameters2D(fixRxns);
+	}
 
-		boolean flagNoParameters = checkNoParameters(fixRxns, noParams);
-		if (!flagNoParameters) {
-			logger.debug("Number of parameters to be fitted provided in INPUT.txt does not equal the number of ones you specified in the optimization section in INPUT.txt!");
+	private static void readMode(BufferedReader in) throws IOException {
+		in.readLine();
+		mode = Integer.parseInt(in.readLine());
+	}
+
+	private static int readOptimizationFlags(BufferedReader in)
+			throws IOException {
+		//number of parameters to be fitted:
+		in.readLine();
+		int noParams = Integer.parseInt(in.readLine());
+
+		//optimization flags:
+		in.readLine();
+		fitting.setFlagRosenbrock(new Boolean(in.readLine()));
+
+
+		in.readLine();
+		fitting.setFlagLM(new Boolean(in.readLine()));
+
+		in.readLine();
+		fitting.setMaxNoEvaluations(new Integer(in.readLine()));
+		return noParams;
+	}
+
+	private static void readNoExperiments(BufferedReader in) throws IOException {
+		//total number of experiments:
+		in.readLine();
+		reactorInputCollector.setTotalNoExperiments(new Integer(in.readLine()));
+
+		//number of experiments in which ignition delay is the response variable
+		in.readLine();
+		reactorInputCollector.setNoIgnitionDelayExperiments(new Integer(in.readLine()));
+		
+		
+		//number of experiments in which flame speed is the response variable
+		in.readLine();
+		reactorInputCollector.setNoFlameSpeedExperiments(new Integer(in.readLine()));
+	}
+
+	private static void readExperimentalDBPaths(BufferedReader in)
+			throws IOException {
+		//Experimental database:(leave blank line if file is not required for run mode)
+		in.readLine();		
+		experiments.setPathExperimentalDB(in.readLine());
+
+		//Ignition Delay database:(leave blank line if file is not required for run mode)
+		in.readLine();
+		experiments.setPathIgnitionDB(in.readLine());
+
+		//flame speed database:(leave blank line if file is not required for run mode)
+		in.readLine();
+		experiments.setPathFlameSpeedDB(in.readLine());
+	}
+
+	private static void readLicenses(BufferedReader in) throws IOException {
+		//Number of chemkin licenses to be used:
+		in.readLine();
+		int noLicenses = Integer.parseInt(in.readLine());
+		licenses = new Licenses(new Integer(noLicenses));
+	}
+
+	private static void readPaths(BufferedReader in) throws IOException {
+		in.readLine();
+		String workingDir = in.readLine();
+		if (workingDir.charAt(workingDir.length()-1)!='/'){
+			logger.debug("Pathname to working directory needs to end with forward slash '/' !");
 			System.exit(-1);
 		}
 		else {
-			//do nothing, continue
+			paths.setWorkingDir(workingDir);
 		}
 
-		in.close();
-
-
-		double [][] betaMin = new double [noFittedReactions][Chemistry.getNoparametersperreaction()];
-		double [][] betaMax = new double [noFittedReactions][Chemistry.getNoparametersperreaction()];
-		for (int i = 0; i < noFittedReactions; i++) {
-			for (int j = 0; j < Chemistry.getNoparametersperreaction(); j++){
-				betaMin[i][j]=0;
-				betaMax[i][j]=1e20;
-			}
-		}
-
-		params.setBetaMin(betaMin);
-		params.setBetaMax(betaMax);
-
-
-		chemistry.setParams(params);
-
-		switch(mode){
-		case 0:	logger.info("PARITY PLOT MODE");	
-		Param_Est p0 = new Param_Est(paths, chemistry, experiments, fitting, licenses);
-		p0.parity();
-		//Function f = new Function(p0.getModelValues(),p0.getExp());
-		//logger.info("SSQ is: "+f.getSRES());
-		break;
-		case 1:	logger.info("PARAMETER OPTIMIZATION MODE");		
-		Param_Est p1 = new Param_Est(paths, chemistry, experiments, fitting, licenses);
-		p1.optimizeParameters();
-		p1.statistics();
-		p1.parity();
-		break;
-		case 2: logger.info("EXCEL POSTPROCESSING MODE");
-		Param_Est p2 = new Param_Est(paths, chemistry, experiments, licenses);
-		p2.excelFiles();
-		break;
-		case 3: logger.info("STATISTICS MODE");
-		Param_Est p3 = new Param_Est(paths, chemistry, experiments, fitting, licenses);
-		p3.statistics();
-		p3.parity();
-		}
-		long timeTook = (System.currentTimeMillis() - time)/1000;
-		logger.info("Time needed for this program to finish: (sec) "+timeTook);
+		//Chemkin Directory:
+		in.readLine();
+		paths.setChemkinDir(in.readLine());
 	}
 
 	/**
