72774949b53f25b9b7420575b59ffe5c30581118
Nick Vandewiele <nickvandewiele@gmail.com>
2012-01-16 17:11:17 +0100

    change visibility of some constants

--- src/parameter_estimation/ChemkinConstants.java
+++ src/parameter_estimation/ChemkinConstants.java
@@ -1,22 +1,22 @@
 package parameter_estimation;
 
 public class ChemkinConstants {
-	static final String CHEMOUT = "chem.out";
-	static final String CHEMASC = "chem.asc";
-	static final String CHEMASU = "chem.asu";
+	public static final String CHEMOUT = "chem.out";
+	public static final String CHEMASC = "chem.asc";
+	public static final String CHEMASU = "chem.asu";
 	
-	static final String TRANOUT = "tran.out";
-	static final String TRANASC = "tran.asc";
+	public static final String TRANOUT = "tran.out";
+	public static final String TRANASC = "tran.asc";
 	
 	static final String CHEMKINDATADTD = "chemkindata.dtd";
 	
 	static final String CKSOLNLIST = "CKSolnList.txt";
 	
-	static final String XML = "XMLdata.zip";
+	public static final String XML = "XMLdata.zip";
 	
-	static final String CKCSVNAME = "CKSoln.ckcsv";
+	public static final String CKCSVNAME = "CKSoln.ckcsv";
 	
 	// input file to CKPreProcess routine:
-	static final String PREPROCESSINPUT = "CKPreProc_template.input";
+	public static final String PREPROCESSINPUT = "CKPreProc_template.input";
 
 }
