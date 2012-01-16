846924a51f4efa6ffca06f7f04aeba0d75b851ad
Nick Vandewiele <nickvandewiele@gmail.com>
2012-01-16 17:06:24 +0100

    ModelValues refactored using factory pattern
    ModelValues type is removed because it was not maintainable.
    Instead, a supertype "ModelValue" is created that has an enum with
    three possibilities. The subclasses of ModelValue specify which type
    of reactor model value is aimed at. More so, for each ModelValue type
    a common but specific method "setValue" to retrieve this info from
    the .ckcsv file is defined.
    
    A factory type "ModelValueFactory" is created in which the flag from
    the ReactorSetupInput is read from the input.xml file. An implementation
    of the ModelValue supertype is then created.
    
    Signed-off-by: Nick Vandewiele <nickvandewiele@gmail.com>

---
+++ src/parameter_estimation/ModelValue.java
@@ -0,0 +1,21 @@
+package parameter_estimation;
+
+import java.io.BufferedReader;
+
+/**
+ * ModelValue is a piece of data we are interested in that emerges from the chemkin simulation.
+ * 
+ * This can either be product effluent fractions, a flame speed, an ignition delay, etc...
+ * 
+ * the container of the data is not specified here because it varies among the model value types.
+ * 
+ * @author nmvdewie
+ *
+ */
+public abstract class ModelValue {
+	public enum TYPE {PRODUCT_EFFLUENT, IGNITION_DELAY, FLAME_SPEED}
+	
+	public TYPE type;
+	
+	public abstract void setValue(BufferedReader bufferedReader);
+}
