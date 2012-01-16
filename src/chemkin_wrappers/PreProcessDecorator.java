666f82d8439c060e8c73ebecbe21f1b8da901e65
Nick Vandewiele <nickvandewiele@gmail.com>
2012-01-16 13:44:08 +0100

    PreProcesesDecorator type added

---
+++ src/chemkin_wrappers/PreProcessDecorator.java
@@ -0,0 +1,64 @@
+package chemkin_wrappers;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import parameter_estimation.ChemkinConstants;
+
+
+public class PreProcessDecorator extends AbstractChemkinRoutine {
+
+	AbstractChemkinRoutine routine;
+
+	public PreProcessDecorator(AbstractChemkinRoutine routine){
+		this.routine = routine;
+	}
+
+	public String[] getKeyword() {
+		String [] input = 
+			{config.paths.getBinDir()+"CKPreProcess",
+				"-i",config.paths.getWorkingDir()+ChemkinConstants.PREPROCESSINPUT};
+		
+		return input;
+	}
+
+	@Override
+	public void executeCKRoutine() {
+		routine.executeCKRoutine();
+		
+	}
+	/**
+	 * create CKPreprocess.input file with directions to chem_inp and output/link files of chemistry and transport:
+	 * @param out TODO
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public void writeCKPreProcessInput()throws IOException, InterruptedException{
+		
+		System.out.println(config.paths.getWorkingDir()+ChemkinConstants.PREPROCESSINPUT);
+		PrintWriter out = new PrintWriter(new FileWriter(config.paths.getWorkingDir()+ChemkinConstants.PREPROCESSINPUT));
+		//in windows: user.dir needs to be followed by "\", in *nix by "/"... 
+		String osname = System.getProperty("os.name");
+		char osChar;
+		if (osname.equals("Linux")){
+			osChar = '/';
+		}
+		else {
+			osChar = '\\';
+		}		
+			out.println("IN_CHEM_INPUT="+config.paths.getWorkingDir()+osChar+config.chemistry.getChemistryInput());
+	
+			//chemistry output, link and species list:
+			out.println("OUT_CHEM_OUTPUT="+config.paths.getWorkingDir()+osChar+ChemkinConstants.CHEMOUT);
+			out.println("OUT_CHEM_ASC="+config.paths.getWorkingDir()+osChar+ChemkinConstants.CHEMASC);
+			out.println("OUT_CHEM_SPECIES="+config.paths.getWorkingDir()+osChar+ChemkinConstants.CHEMASU);
+	
+			//transport link files and log file:
+			out.println("FIT_TRANSPORT_PROPERTIES=1");
+			out.println("OUT_TRAN_OUTPUT="+config.paths.getWorkingDir()+osChar+ChemkinConstants.TRANOUT);
+			out.println("OUT_TRAN_ASC="+config.paths.getWorkingDir()+osChar+ChemkinConstants.TRANASC);
+	
+			out.close();
+	}
+}
