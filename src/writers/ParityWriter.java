eface44740bc2a0cf852207127912bf51b070097
nickvandewiele <nickvandewiele@gmail.com>
2013-07-16 14:51:36 +0200

    create writers package

---
+++ src/writers/ParityWriter.java
@@ -0,0 +1,108 @@
+package writers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import parameter_estimation.Paths;
+
+import datatypes.EffluentExperimentalValue;
+import datatypes.EffluentModelValue;
+import datatypes.ExperimentalValue;
+import datatypes.FlameSpeedExperimentalValue;
+import datatypes.FlameSpeedModelValue;
+import datatypes.IgnitionDelayExperimentalValue;
+import datatypes.IgnitionDelayModelValue;
+import datatypes.ModelValue;
+
+public class ParityWriter {
+
+	Paths paths;
+
+	ExperimentalValue[] experimentalValues;
+	ModelValue[] modelValues;
+	List<String> speciesNames;
+	public ParityWriter(Paths paths, ExperimentalValue[] experimentalValues,
+			ModelValue[] modelValues, List<String> speciesNames) {
+		this.paths = paths;
+		this.experimentalValues = experimentalValues;
+		this.modelValues = modelValues;
+		this.speciesNames = speciesNames;
+	}
+
+	public void write() throws IOException {
+		writeSpeciesFractions();
+		writeIgnitionDelays();
+		writeFlameSpeeds();;
+
+	}
+
+	private void writeSpeciesFractions() {
+		String s = " , ,"+"Experimental Value ,"+"Model Value ,"+"Experimental Value\n";
+		for(int j=0;j<speciesNames.size();j++){//order per species: all experiments
+			for(int i = 0; i < experimentalValues.length; i++){
+				ExperimentalValue experimentalValue = experimentalValues[i];
+				ModelValue modelValue = modelValues[i];
+
+				if(experimentalValue.type.equals(ExperimentalValue.PRODUCT_EFFLUENT)){
+					Double experiment_value = ((EffluentExperimentalValue)experimentalValue).speciesFractions.get(speciesNames.get(j));
+					Double model_value = ((EffluentModelValue)modelValue).getSpeciesFractions().get(speciesNames.get(j));
+					//out.println(speciesNames.get(i));
+					if(model_value != null && experiment_value != null){
+						s+=speciesNames.get(j).toString()+",";//comma delimited
+						s+="experiment no. "+(i+1)+","+experiment_value+","+model_value+","+experiment_value+"\n";
+					}
+				}
+			}
+		}
+		File file = new File(paths.getWorkingDir()+"SpeciesParity.csv");
+		if(!s.equals(" , ,"+"Experimental Value ,"+"Model Value ,"+"Experimental Value\n")) write(file,s);
+
+	}
+
+	private void writeFlameSpeeds() {
+		String s = "";
+
+		for(int i = 0; i < experimentalValues.length; i++){
+			ExperimentalValue experimentalValue = experimentalValues[i];
+			ModelValue modelValue = modelValues[i];
+
+			if(experimentalValue.type.equals(ExperimentalValue.FLAME_SPEED)){
+				s+="Flame Speed:\t"+((FlameSpeedExperimentalValue)experimentalValue).value+"\t"+((FlameSpeedModelValue)modelValue).value+","+((FlameSpeedExperimentalValue)experimentalValue).value+"\n";
+			}
+		}
+		File file = new File(paths.getWorkingDir()+"FlameSpeedParity.csv");
+		if(!s.isEmpty()) write(file,s);
+
+	}
+
+	private void writeIgnitionDelays() {
+		String s = "";
+
+		for(int i = 0; i < experimentalValues.length; i++){
+			ExperimentalValue experimentalValue = experimentalValues[i];
+			ModelValue modelValue = modelValues[i];
+
+			if(experimentalValue.type.equals(ExperimentalValue.IGNITION_DELAY)){
+				s+="Ignition Delay:\t"+((IgnitionDelayExperimentalValue)experimentalValue).value+"\t"+((IgnitionDelayModelValue)modelValue).value+","+((IgnitionDelayExperimentalValue)experimentalValue).value+"\n";
+			}
+		}
+		File file = new File(paths.getWorkingDir()+"IgnitionDelayParity.csv");
+		if(!s.isEmpty()) write(file,s);
+
+	}
+
+	private void write(File file, String s) {
+
+		try {
+			FileUtils.writeStringToFile(file, s);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
