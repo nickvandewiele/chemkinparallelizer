3627973942aa6caf3c5c9f1fdf855e1e1d382bba
nmvdewie <nickvandewiele@gmail.com>
2013-09-19 13:46:51 +0200

    change name of jar to chemkinparallelizer in examples

--- examples/Batch/runNBMT.bat
+++
@@ -1,2 +0,0 @@
-#fill in the right name of the jar: (instead of nbmt.jar)
-java -jar nbmt.jar --mass INPUT.xml
---
+++ examples/mixed_sims/run.bat
@@ -0,0 +1,2 @@
+#fill in the right name of the jar: (instead of chemkinparallelizer.jar)
+java -jar chemkinparallelizer.jar --mass INPUT.xml
