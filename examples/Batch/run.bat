4e72fb67c562fe3b3c54ca65c8b9975474c0879d
nickvandewiele <nickvandewiele@gmail.com>
2013-11-20 16:15:25 +0100

    update bat files of example folder
    replace # by @REM
    
    closes #5

--- examples/Batch/run.bat
+++ examples/Batch/run.bat
@@ -1,2 +1,2 @@
-#fill in the right name of the jar: (instead of chemkinparallelizer.jar)
+@REM fill in the right name of the jar: (instead of chemkinparallelizer.jar)
 java -jar chemkinparallelizer.jar --mass INPUT.xml
